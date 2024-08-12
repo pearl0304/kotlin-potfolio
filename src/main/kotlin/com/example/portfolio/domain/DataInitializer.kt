ab330daefab8261fa2354f7429f20fcd85788ffc
kylie <l.kylie4630@gmail.com>
2024-08-12 22:04:59 +0900

    Ing : DataInitialize

---
+++ src/main/kotlin/com/example/portfolio/domain/DataInitializer.kt
@@ -0,0 +1,23 @@
+package com.example.portfolio.domain
+
+import com.example.portfolio.domain.repository.*
+import jakarta.annotation.PostConstruct
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+@Component
+@Profile(value = ["default"])
+class DataInitialize(
+  private val achievementRepository: AchievementRepository,
+  private val introductionRepository: IntroductionRepository,
+  private var linkRepository: LinkRepository,
+  private val skillRepository: ProjectSkillRepository,
+  private val projectRepository: ProjectRepository,
+  private val experienceRepository: ExperienceRepository
+) {
+
+  @PostConstruct
+  fun initializeData() {
+
+  }
+}
