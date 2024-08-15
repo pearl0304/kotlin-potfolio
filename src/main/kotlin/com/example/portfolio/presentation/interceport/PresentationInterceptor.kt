26cd6440221317d52efa7c2b2616cf9c0bf8b076
kylie <l.kylie4630@gmail.com>
2024-08-15 21:53:13 +0900

    feat: Create interceptor

--- src/main/kotlin/com/example/portfolio/presentation/interceport/PresentationInterceptor.kt
+++ src/main/kotlin/com/example/portfolio/presentation/interceport/PresentationInterceptor.kt
@@ -1,6 +1,22 @@
 package com.example.portfolio.presentation.interceport
 
+import com.example.portfolio.domain.entity.HttpInterface
+import com.example.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
+// 컨트롤러 작동 후에 실행
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+  private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+  override fun afterCompletion(
+    request: HttpServletRequest, response: HttpServletResponse,
+    handler: Any, ex: Exception?
+  ) {
+    val httpInterface = HttpInterface(request)
+    httpInterfaceRepository.save(httpInterface)
+  }
+}
