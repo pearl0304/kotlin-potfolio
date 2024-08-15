2f462ff7176652d38d9cb85b4f9f83448fac90dc
kylie <l.kylie4630@gmail.com>
2024-08-15 16:47:49 +0900

    feat: Create TestCode

--- src/main/kotlin/com/example/portfolio/presentation/servcie/PresentationService.kt
+++ src/main/kotlin/com/example/portfolio/presentation/servcie/PresentationService.kt
@@ -1,6 +1,47 @@
 package com.example.portfolio.presentation.servcie
 
+import com.example.portfolio.presentation.dto.IntroductionDTO
+import com.example.portfolio.presentation.dto.LinkDTO
+import com.example.portfolio.presentation.dto.ProjectDTO
+import com.example.portfolio.presentation.dto.ResumeDTO
+import com.example.portfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PresentationService
+class PresentationService(
+  private val presentationRepository: PresentationRepository
+
+) {
+
+  @Transactional(readOnly = true)
+  fun getIntroduction(): List<IntroductionDTO> {
+    val introductions = presentationRepository.getActiveIntroductions()
+    return introductions.map { introduction -> IntroductionDTO(introduction) }
+  }
+
+  @Transactional(readOnly = true)
+  fun getLink(): List<LinkDTO> {
+    val links = presentationRepository.getActiveLinks()
+    return links.map { link -> LinkDTO(link) }
+  }
+
+  @Transactional(readOnly = true)
+  fun getResume(): ResumeDTO {
+    val experiences = presentationRepository.getActiveExperiences()
+    val achievements = presentationRepository.getActiveAchievements()
+    val skills = presentationRepository.getActiveSkills()
+
+    return ResumeDTO(
+      experiences = experiences,
+      achievements = achievements,
+      skills = skills
+    )
+  }
+
+  @Transactional(readOnly = true)
+  fun getProjects(): List<ProjectDTO> {
+    val projects = presentationRepository.getActiveProjects()
+    return projects.map { project -> ProjectDTO(project) }
+  }
+}
---
+++ src/test/kotlin/com/example/portfolio/presentation/servcie/PresentationServiceTest.kt
@@ -0,0 +1,75 @@
+package com.example.portfolio.presentation.servcie
+
+import com.example.portfolio.domain.entity.Introduction
+import com.example.portfolio.domain.entity.Link
+import com.example.portfolio.presentation.repository.PresentationRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class PresentationServiceTest {
+  @InjectMocks
+  lateinit var presentationService: PresentationService
+
+  @Mock
+  lateinit var presentationRepository: PresentationRepository
+
+  var DATA_SIZE = 9
+
+  @Test
+  fun testGetIntroductions() {
+    // given
+    val introductions = mutableListOf<Introduction>()
+    for (i in 1..DATA_SIZE) {
+      val introduction = Introduction(content = "${i}", isActive = i % 2 == 0)
+      introductions.add(introduction)
+    }
+    val activeIntroductions = introductions.filter { it.isActive }
+
+    Mockito.`when`(presentationRepository.getActiveIntroductions())
+      .thenReturn(activeIntroductions)
+
+    // when
+    val introductionDTOs = presentationService.getIntroduction()
+
+    // then
+    assertThat(introductionDTOs).hasSize(DATA_SIZE / 2)
+    for (introductionDTO in introductionDTOs) {
+      assertThat(introductionDTO.content.toInt()).isEven()
+    }
+  }
+
+  @Test
+  fun testGetLinks() {
+    // given
+    val links = mutableListOf<Link>()
+    for (i in 1..DATA_SIZE) {
+      val link = Link(name = "${i}", content = "${i}", isActive = i % 2 != 0)
+      links.add(link)
+    }
+    val activeLinks = links.filter { link ->
+      link.isActive
+    }
+    Mockito.`when`(presentationRepository.getActiveLinks())
+      .thenReturn(activeLinks)
+
+    // when
+    val linkDTOs = presentationService.getLink()
+
+    // then
+    var expectedSize = DATA_SIZE / 2
+    if (DATA_SIZE % 2 != 0) {
+      expectedSize += 1
+    }
+    assertThat(linkDTOs).hasSize(expectedSize)
+    for (linkDTO in linkDTOs) {
+      assertThat(linkDTO.content.toInt()).isOdd()
+    }
+  }
+
+}
